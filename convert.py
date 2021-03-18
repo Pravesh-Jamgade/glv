5a1764d06bf987323ccc04e8cd39e211ba74646c
alpeshjamgade <alpeshjamgade21@gmail.com>
2021-03-19 03:53:47 +0530

    can visualize pointcloud of google location history

--- convert.py
+++ convert.py
@@ -4,6 +4,9 @@ import pandas as pd
 import numpy as np
 import pptk
 
+# for nagpur india
+utm_zone = 44
+#IN_VEHICLE, STILL, UNKNOWN, TILTING, ON_BICYCLE
 def convert(data):
 	locations = data['locations']
 	with open(args.output, 'w') as csvfile:
@@ -35,29 +38,20 @@ def convert(data):
 
 	print('Done! Wrote ' + str(len(locations)) + ' points.')
 
-
-def process():
-    points = pd.read_csv(plt_file, skiprows=6, header=None,
-                         parse_dates=[[5, 6]], infer_datetime_format=True)
-
-    # for clarity rename columns
-    points.rename(inplace=True, columns={'5_6': 'time', 0: 'lat', 1: 'lon', 3: 'alt', 4: 'label'})
-
-    # remove unused columns
-    points.drop(inplace=True, columns=[2, 4])
-
-    return points
-
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Convert Google Maps location history to csv file.')
 	parser.add_argument('-i', '--input', action="store", help='Filepath to location history .json file.', required=True)
 	parser.add_argument('-o', '--output', action="store", help='Filepath to write output .csv file.', required=True)
 
+	utm_zone = float(input("enter utm_zone: "))
 	args = parser.parse_args()
 	file = open(args.input)
 	data = json.load(file)
 	convert(data)
 	df = pd.read_csv(args.output)
-	p = np.c_[ df['lon', df['lat'], np.zeros(len(df))]]
+	p = np.c_[ df['longitude'], df['latitude'], np.zeros(len(df))]
 	v = pptk.viewer(p)
-	v.attribute(df['alt'])
+	v.attribute(df['type'])
+	v.color_map('jet', scale=[0, 2000])
+
+
